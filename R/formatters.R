abb0be4cc6d20d854dd03534d66c4697b2c84ea1
Aaron Duke <aaron@0ptimus.com>
2014-10-09 18:25:27 -0400

    updated totitle to handle McKeee

--- R/formatters.R
+++ R/formatters.R
@@ -9,5 +9,8 @@ pN <- function(x){prettyNum(x, big.mark = ",")}
 
 #' @describeIn formatters
 #' @export
-totitle <- function(x){gsub("\\b([a-z])([a-z]+)", "\\U\\1\\L\\2" ,x, perl=TRUE)}
-
+totitle <- function(x){
+  x <- gsub("(?i)\\b([a-z])([a-z]+)", "\\U\\1\\L\\2" ,x, perl=TRUE)
+  x <- gsub("\\bMc([bcgldkmcp])", "Mc\\U\\1\\E", x, perl=T)
+  gsub("\\b(I[iv]+)\\b", "\\U\\1\\E", x, perl=T)
+}
