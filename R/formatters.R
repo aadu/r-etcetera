afd99582a6efac91e8782863e91f77bc67fb80a7
Aaron Duke <aaron@0ptimus.com>
2014-10-09 16:41:25 -0400

    updated totitle.

--- R/formatters.R
+++ R/formatters.R
@@ -1,6 +1,13 @@
 #' Pretty Num with a comma as big.mark
 #'
-#' @param x Any numeric value
-#' @return Prettified number
+#' @export
+formatters <- function(x)(return(x))
+
+#' @describeIn formatters
 #' @export
 pN <- function(x){prettyNum(x, big.mark = ",")}
+
+#' @describeIn formatters
+#' @export
+totitle <- function(x){gsub("\\b([a-z])([a-z]+)", "\\U\\1\\L\\2" ,x, perl=TRUE)}
+
